d8a2e352aa1dba532618dcc8cd9c7292e942ff1c
maxim12313 <maximkim16@gmail.com>
2024-12-20 17:29:50 -0800

    extract phone and email

--- parser.py
+++ parser.py
@@ -1,6 +1,25 @@
 import sys
 import spacy
+import re
 from pypdf import PdfReader
+
+
+def getPhone(text):
+    # https://regex101.com/library/wZ4uU6?orderBy=RELEVANCE&search=phone
+    expr = r"(?:([+]\d{1,4})[-.\s]?)?(?:[(](\d{1,3})[)][-.\s]?)?(\d{1,4})[-.\s]?(\d{1,4})[-.\s]?(\d{1,9})"
+    matches = re.findall(expr, text)
+    if len(matches):
+        return " ".join(x for x in matches[0] if x)
+    return ""
+
+
+def getEmail(text):
+    # https://regex101.com/library/mX1xW0?orderBy=RELEVANCE&search=email
+    expr = r"([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)"
+    matches = re.findall(expr, text)
+    if len(matches):
+        return matches[0][0] + "@" + ".".join(matches[0][1:])
+    return ""
 
 
 def readPdf(path):
@@ -16,4 +35,3 @@ if __name__ == "__main__":
         exit(1)
     path = sys.argv[1]
     text = readPdf(path)
-    print(text)
